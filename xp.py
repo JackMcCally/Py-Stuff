43859c1ddde5dc6d5c315f6235580b41d4f227bf
JackMcCally <jacksta414@gmail.com>
2016-11-12 13:36:58 -0600

    Hyplex Build Random Gen
    HyplexJack

---
+++ xp.py
@@ -0,0 +1,15 @@
+import random
+
+print("Starting random gen!")
+print("Please wait!")
+
+rint = random.randint(1,10)
+
+print( "Random Number: " + str(rint) )
+
+if rint > 5:
+    print("Build a castle")
+if rint <5:
+    print("Build Pepe!")
+
+print("Done")
